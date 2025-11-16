47c1f1b0cee85d62de9611104119dc7296692919
Kobiiiii <89201475+Kobiiiii@users.noreply.github.com>
2025-11-16 22:04:40 +0000

    Delete HTML & CSS/Resillience ESports (WORK IN PROGRESS)/Resillience E-Sports (1) directory

--- HTML & CSS/Resillience ESports (WORK IN PROGRESS)/Resillience E-Sports (1)/script.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.getElementsByClassName("navbar");
-const video = document.getElementsByClassName("container");
-
-document.addEventListener("scroll", navbarDetect)
-
-function navbarDetect(){
-if(window.scrollY > video[0].offsetHeight){navbar[0].style.backgroundColor = "black"} else {navbar[0].style.backgroundColor = "transparent"}
-}
